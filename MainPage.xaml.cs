6f6cbbcc8b57003720679f00b83638d7973d7396
Diogo Santos Teixeira <101653810+diogotei3@users.noreply.github.com>
2023-08-20 11:06:19 -0300

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Windows.Services.Store;
+
+namespace TP01
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSubmit(object sender, EventArgs e)
+        {
+
+            if (id.Text == "admin" && password.Text == "senha@dmin")
+                DisplayAlert("Logado com sucesso!", "", "ok");
+            else
+                DisplayAlert("Não autorizado!", "Digite o login de adminstrador", "ok");
+        }
+
+        private void OnClear(object sender, EventArgs e)
+        {
+            id.Text = "";
+            password.Text = "";
+        }
+
+        private void OnCredits(object sender, EventArgs e) 
+        {
+            DisplayAlert("Realizado por", "Diogo Santos Teixeira\nGuilherme Ferreira", "ok");
+        }
+    }
+}
